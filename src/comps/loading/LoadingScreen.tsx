79d0f22deb83e5e590c807996a3bd0bc4e826eb4
lukas vidfar <dev@arcadian.se>
2023-05-30 12:14:00 +0400

    2023-05-30

---
+++ src/comps/loading/LoadingScreen.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from "react";
+
+import s from "./LoadingScreen.module.css";
+
+const LoadingScreen = () => {
+  useEffect(() => {
+    const complete = document.readyState === "complete";
+    if (complete) {
+      const loadingScreen = document.getElementById("loadingScreen");
+      if (loadingScreen) {
+        setTimeout(() => {
+          const fadeOut = loadingScreen.animate(
+            [
+              {
+                opacity: 1,
+              },
+              {
+                opacity: 0,
+              },
+            ],
+            {
+              duration: 1000,
+              fill: "forwards",
+            }
+          );
+          fadeOut.onfinish = () => {
+            loadingScreen.remove();
+          };
+        }, 2500);
+      }
+    }
+  }, []);
+  return (
+    <div id="loadingScreen" className={s.section}>
+      <section className={s.container}>
+        <div className={s.loaderOuter}>
+          <div className={s.loaderInner} />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default LoadingScreen;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,8 @@ import "@/styles/globals.css";
 
 import Head from "next/head";
 import { ReactLenis } from "@studio-freight/react-lenis";
+import LoadingScreen from "@/comps/loading/LoadingScreen";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -15,6 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
       </Head>
       <ReactLenis root>
+        <LoadingScreen />
         <Component {...pageProps} />
       </ReactLenis>
       <Analytics />
